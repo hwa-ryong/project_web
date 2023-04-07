4fc08932f71c18d020a57de72222f7eb3256b569
hwa-ryong <hwa-ryong@gmail.com>
2023-04-07 17:39:53 +0900

    cafe12

---
+++ cafe12/js/main-js.js
@@ -0,0 +1,31 @@
+window.onload = function(){
+
+//디지털 시계
+setInterval(setWatch, 1000);
+
+function setWatch(){
+    const date = new Date();
+    let now = date.toLocaleTimeString();
+    document.getElementById("demo").innerHTML = now;
+}
+
+//배경 이미지 change
+let picture = ["./images/header1.jpg", "./images/header2.jpg", 
+               "./images/header3.jpg"];
+
+let imgIdx = 0;
+
+showPicture(); //함수 호출
+
+function showPicture(){
+   let img = document.getElementById("pic");
+   img.src = picture[imgIdx];
+   imgIdx++;
+
+   if(imgIdx == picture.length) {
+        imgIdx = 0;
+   }
+
+   setTimeout(showPicture, 1000);
+}
+}
